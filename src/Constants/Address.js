8f12b2cef0de68cf00b05e41f2cb0b8afb6615dc
seed-ganesh <ecofriendganesh@gmail.com>
2020-10-04 12:47:34 +0530

    address commited

--- src/Constants/Address.js
+++ src/Constants/Address.js
@@ -1,10 +1,8 @@
 export default `
 <address>
     Seed Ganesha,<br/>
-    Level 7, IIFL Towers, 143,
-    MGR Main Road, Kandancavadi,
-    Perungudi,
-    Chennai,
-    Tamilnadu 600096.<br/>
-    PH/Mble - 9176636367.
+    XXXX,
+    YYYY,
+    ZZZZ,<br/>
+    PH/Mble - AAAAAAA.
 </address>`
